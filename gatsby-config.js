76bb2066740c540f2f6a6dac17d695d62849bf26
allisonkydy <allisnelly@gmail.com>
2020-03-09 15:50:30 -0500

    basic config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Allison Donnelly`,
+    description: `Allison Donnelly is a full stack web developer and a passionate problem-solver.`,
+    author: `@allisonkydy`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,11 +18,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Allison Donnelly`,
+        short_name: `Allison Donnelly`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#f4f4f4`,
+        theme_color: `#00ba94`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
